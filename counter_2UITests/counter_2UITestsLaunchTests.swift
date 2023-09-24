6a454c1ff94f308be3ba4ec80852f7f095ce3526
animacomedentis <maximmangk5@gmail.com>
2023-09-24 12:05:21 +0300

    Initial Commit

---
+++ counter_2UITests/counter_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  counter_2UITestsLaunchTests.swift
+//  counter_2UITests
+//
+//  Created by Максим Петров on 24.09.2023.
+//
+
+import XCTest
+
+final class counter_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
