0956e3107b52c2bf0f8f620bd02a3d27016bfd45
animacomedentis <maximmangk5@gmail.com>
2023-10-01 01:54:54 +0300

    update

--- counter_2/ViewController.swift
+++ counter_2/ViewController.swift
@@ -14,49 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak private var reset: UIButton!
     @IBOutlet weak private var minus: UIButton!
     @IBOutlet weak private var plus: UIButton!
-    // функция форматирующая дату
-    private func date() -> String{
-        let currentDate = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM/d yyyy, HH:mm "
-        return dateFormatter.string(from: currentDate)
-    }
-    var value = 0
-    // функция увеличивает счетчик на 1 и выводит в сообщение на экран приложения
-    private func incrementCounter(){
-        value += 1
-        result.text = String(value)
-        console.text += "\n[\(date())]: значение изменено на +1"
-    }
-    // функция уменьшает счетчик на 1, если оно больше 0 и выводит в сообщение на экран приложения
-    private func decrementCounter(){
-        if value == 0{
-            result.text = String(value)
-            console.text += "\n[\(date())]: попытка уменьшить значение счётчика ниже 0"
-        }else if value > 0{
-            value -= 1
-            result.text = String(value)
-            console.text += "\n[\(date())]: значение изменено на -1"
-        }
-    }
-    // функция сбрасыват счетчик и выводит соответсвующее сообщение
-    private func resetCounter(){
-        value = 0
-        result.text = String(value)
-        console.text += "\n[\(date())]: значение сброшено"
-    }
-    //функция scroll для окна истории обновлений
-    private func scroll(){
-        let range = NSRange(location: -1, length: 1)
-        console.scrollRangeToVisible(range)
-    }
     
- 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-    }
+    private var value = 0
+
+    //MARK: - Public methods
+    
     @IBAction private func increment(_ sender: Any) {
         incrementCounter()
         scroll()
@@ -71,5 +33,46 @@ class ViewController: UIViewController {
         resetCounter()
         scroll()
     }
+    
+    // MARK: - Private methods
+    
+    // функция форматирующая дату
+    private func currentDate() -> String {
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM/d yyyy, HH:mm "
+        return dateFormatter.string(from: currentDate)
+    }
+    
+    // функция увеличивает счетчик на 1 и выводит в сообщение на экран приложения
+    private func incrementCounter() {
+        value += 1
+        result.text = String(value)
+        console.text += "\n[\(currentDate())]: значение изменено на +1"
+    }
+    // функция уменьшает счетчик на 1, если оно больше 0 и выводит в сообщение на экран приложения
+    private func decrementCounter(){
+        if value == 0 {
+            result.text = String(value)
+            console.text += "\n[\(currentDate())]: попытка уменьшить значение счётчика ниже 0"
+        } else if value > 0 {
+            value -= 1
+            result.text = String(value)
+            console.text += "\n[\(currentDate())]: значение изменено на -1"
+        }
+    }
+    // функция сбрасыват счетчик и выводит соответсвующее сообщение
+    private func resetCounter(){
+        value = 0
+        result.text = String(value)
+        console.text += "\n[\(currentDate())]: значение сброшено"
+    }
+    
+    //функция scroll для окна истории обновлений
+    private func scroll(){
+        let range = NSRange(location: -1, length: 1)
+        console.scrollRangeToVisible(range)
+    }
+
 }
  
